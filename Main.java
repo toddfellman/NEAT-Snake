c08855f096e2a6bf2a629a20e054192d35b8ae98
toddfellman <60018341+toddfellman@users.noreply.github.com>
2020-02-19 09:39:42 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,18 +9,30 @@ class Main {
    
    public static Scanner scan = new Scanner(System.in);
    
+   public static final Thread t = new Thread();
+   
    public static void main(String[] args) {
       window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setTitle("NEAT snake");
       
       
-      window.setSize(Board.WIDTH * Board.PIXEL_SIZE, Board.HEIGHT * Board.PIXEL_SIZE);
+      window.setSize(Board.WIDTH * Board.PIXEL_SIZE, Board.HEIGHT * Board.PIXEL_SIZE + Board.PIXEL_SIZE);
 		window.setResizable(false);
       window.setVisible(true);
       
-      Population p = new Population(100);
-      p.getOneMatch(window.getGraphics());
       
+      Population pop = new Population(128);
+      while (true) {
+         pop.getOneMatch();
+      }
+   }
+   
+   public static void pause(long time) {
+      try {
+         t.sleep(time);
+      } catch (InterruptedException e) {
+         System.out.println(e);
+      }
    }
 }
